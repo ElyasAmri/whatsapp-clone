a65c75754b4cb1e195c38c5a63b58cf464ab4f6a
ElyasAmri <elyas.amri@gmail.com>
2022-01-29 23:24:25 +0300

    Add chat list and chat list item

---
+++ components/ChatListItem.tsx
@@ -0,0 +1,73 @@
+import {
+  Image, Platform,
+  StyleSheet,
+  Text,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import {useNavigation} from '@react-navigation/native'
+import moment from 'moment'
+import type {ChatListItemProps} from '../types'
+
+export default function ChatListItem({id, name, image, lastMessage}: ChatListItemProps) {
+  const navigation = useNavigation()
+
+  const content = (
+      <View style={styles.container}>
+        <Image source={{uri: image}} style={styles.image}/>
+        <View style={styles.data}>
+          {/*<Text>{id}</Text>*/}
+          <Text style={styles.name}>{name}</Text>
+          {lastMessage &&
+              <>
+                <Text style={styles.lastMessage}>{lastMessage.content}</Text>
+                {/*TODO: fix moment data format issue*/}
+                <Text style={styles.timestamp}>{moment(lastMessage.timestamp).format('DD/MM/YYYY')}</Text>
+              </>
+          }
+        </View>
+      </View>
+  )
+
+  const chatPressed = () => {
+    navigation.navigate('ChatScreen', {id: id, name: name})
+  }
+
+  return Platform.OS === 'android'
+      ? <TouchableNativeFeedback onPress={chatPressed} children={content}/>
+      : <TouchableOpacity onPress={chatPressed} children={content}/>
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    height: 80,
+    alignItems: 'center',
+    paddingHorizontal: 15,
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    marginRight: 15,
+  },
+  data: {
+    position: 'relative',
+    flex: 1,
+  },
+  name: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  lastMessage: {
+    color: '#ababab',
+    fontSize: 16,
+  },
+  timestamp: {
+    position: 'absolute',
+    top: 4,
+    right: 0,
+    fontSize: 12,
+  },
+})
--- screens/ChatsListScreen.tsx
+++ screens/ChatsListScreen.tsx
@@ -1,3 +1,24 @@
+import {FlatList, ListRenderItem} from 'react-native'
+import _ from 'lodash'
+
+import {useFetchChatList} from '../hooks/data'
+import ChatListItem from '../components/ChatListItem'
+import type {Chat} from '../types'
+
 export default function ChatsListScreen() {
-  return null
+  const chatList = useFetchChatList('u1')
+
+  const renderItem: ListRenderItem<Chat> = ({item: chat}) => (
+      <ChatListItem {...chat} lastMessage={_.last(chat.messages)}/>
+  )
+
+  const keyExtractor = (item: Chat) => item.id
+
+  return (
+      <FlatList
+          data={chatList}
+          renderItem={renderItem}
+          keyExtractor={keyExtractor}
+      />
+  )
 }
