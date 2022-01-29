191c291b32f5411ffd772e28b08792d48d141498
ElyasAmri <elyas.amri@gmail.com>
2022-01-29 21:04:26 +0300

    Add and Remove Screens + index declarations

---
+++ screens/index.ts
@@ -0,0 +1,15 @@
+import CallsScreen from './CallsScreen'
+import CameraScreen from './CameraScreen'
+import ChatsListScreen from './ChatsListScreen'
+import ModalScreen from './ModalScreen'
+import NotFoundScreen from './NotFoundScreen'
+import StatusScreen from './StatusScreen'
+
+export {
+  CallsScreen,
+  CameraScreen,
+  ChatsListScreen,
+  StatusScreen,
+  NotFoundScreen,
+  ModalScreen,
+}
